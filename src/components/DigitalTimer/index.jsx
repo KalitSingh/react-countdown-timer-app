95fe81bcd7dd7ab6b7701810f10915d33a81723e
Kalit Singh <kalitsingh9935@gmail.com>
2026-03-24 15:29:04 +0530

    Reset button logic implemented, setting Timer Limit control button, input box for value with fixed issue of negative input value, handled disabling button at timer running state and for negative input value

--- src/components/DigitalTimer/index.jsx
+++ src/components/DigitalTimer/index.jsx
@@ -6,14 +6,48 @@ class DigitalTimer extends Component {
         super(props)
         this.state = {
             timeElapsedInSec: 0,
-            timerLimitInMin: 1,
+            timerLimitInMin: 5,
             isTimerRunning: false
         }
     }
 
+    // Click event of +(incre timer) button on Timer Limit controller 
+    onClickPlusIncre = () => {
+        this.setState(prevState => ({timerLimitInMin: prevState.timerLimitInMin + 1}))
+    }
+
+     // Click event of -(decre timer) button on Timer Limit controller 
+    onClickMinusDecre = (event) => {
+        const {timerLimitInMin} = this.state
+
+        if (timerLimitInMin < 0){
+            alert("Timer Value Can't Be Negative")
+        } else{
+            this.setState(prevState => ({timerLimitInMin: prevState.timerLimitInMin - 1}))
+        }
+        
+    }
+    
+    // + and - 
     renderTimerLimitController = () => {
+        const {timerLimitInMin, isTimerRunning} = this.state 
+        console.log(timerLimitInMin < 0)
+        
+        // minusButtonDisalbed is for handling -ve input number from user
+        const isMinusButtonDisabled = (isTimerRunning || (timerLimitInMin <= 0)) ? true : false;
+        const isButtonDisabled = isTimerRunning ? true : false;
+
         return(
-            <h1>controls btn</h1>
+            <>
+            <p className="timer-mimit-title">Set Timer</p>
+            <div className="timer-limit-controller-container">
+                <button disabled={isButtonDisabled} onClick={this.onClickPlusIncre} className="timer-limit-button">+</button>
+                <div>
+                    <input className="timerLimitInputValue" value={timerLimitInMin} type="number" />
+                </div>
+                <button disabled={isMinusButtonDisabled} onClick={this.onClickMinusDecre} className="timer-limit-button">-</button>
+            </div>
+            </>
         )
     }
 
@@ -23,6 +57,7 @@ class DigitalTimer extends Component {
 
         const isTimerCompleted = timerLimitInMin * 60 === timeElapsedInSec
 
+        // System Action(Internal Logic of Timer): Agar timer pura hua to incrementing time ko rok do incrment hone se
         if (isTimerCompleted){
             clearInterval(this.timerId)
         } 
@@ -31,10 +66,22 @@ class DigitalTimer extends Component {
         }
     }
 
+    // This is for - when reset button is clicked 
+    onResetTimer = () => {
+        const {timerLimitInMin} = this.state
+
+        clearInterval(this.timerId)
+        this.setState({timerLimitInMin: 45, timeElapsedInSec: 0, isTimerRunning: false})
+        
+    }
+
+    // This below is for - When Start or Pause Button Clicked
     onStartOrPauseTimer = () => {
     const {isTimerRunning, timeElapsedInSec, timerLimitInMin} = this.state 
 
     const isTimerCompleted = timeElapsedInSec === timerLimitInMin * 60;
+
+    // User Action (For UI): To Reset Timer Seconds from Elapsed to 1 in UI
     if (isTimerCompleted) {
         clearInterval(this.timerId)
         this.setState({timeElapsedInSec: 1})
@@ -63,7 +110,9 @@ class DigitalTimer extends Component {
                     <img className="timer-controller-icon" src={startOrPauseImgUrl} />
                     <p className="timer-controller-label">{isTimerRunning ? "Pause" : "Start"}</p>
                 </button>
-                <button className="timer-controller-btn">
+                <button 
+                    onClick={this.onResetTimer}
+                    className="timer-controller-btn">
                     <img className="timer-controller-icon" src="https://res.cloudinary.com/dkr51xeft/image/upload/v1774177891/react-projects/react-countdown-timer-app/reset-icon-img_mwtgkt.png" />
                     <p className="timer-controller-label">Reset</p>
                 </button>
